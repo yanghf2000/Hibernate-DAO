a26307b9933ac20c9cab931439f8b2209072c161
Huifeng_Yang <yaghf2000@163.com>
2021-03-26 14:56:15 +0800

    update

--- src/main/java/com/github/yanghf2000/analyzer/ChineseAnalysisConfigurer.java
+++ src/main/java/com/github/yanghf2000/analyzer/ChineseAnalysisConfigurer.java
@@ -1,10 +1,10 @@
-package analyzer;
+package com.github.yanghf2000.analyzer;
 
 import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
 
-public class AnalysisConfigurer implements LuceneAnalysisConfigurer {
+public class ChineseAnalysisConfigurer implements LuceneAnalysisConfigurer {
     @Override
     public void configure(LuceneAnalysisConfigurationContext context) {
         context.analyzer("chinese").instance(new SmartChineseAnalyzer());
